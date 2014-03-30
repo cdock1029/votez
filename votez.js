ec29df3d478c5188a70c38f3663beebc06f176a7
Conor Dockry <conordockry@gmail.com>
2014-03-29 17:50:19 -0700

    incremental

---
+++ votez.js
@@ -0,0 +1,53 @@
+Places = new Meteor.Collection("places");
+Meteor.methods({
+    vote: function(placeId) {
+        var place = Places.findOne(placeId);
+        Places.update(placeId, {$set: {votes: place.votes + 1}});
+    },
+    reset: function() {
+        Places.update({}, {$set: {votes: 0}}, {multi: true});
+    }
+
+});
+if (Meteor.isClient) {
+    Meteor.subscribe("places");
+
+    Template.placesList.places = function () {
+        return Places.find({}, {sort: {votes: -1, name: 1}});
+    };
+
+    Template.placesList.events({
+        "click .vote": function() {
+            Meteor.call("vote", this._id);
+        }
+    });
+
+    Template.header.events({
+        "click .reset": function() {
+            Meteor.call("reset");
+        }
+    });
+
+    Template.newPlace.events({
+        "submit .newPlaceForm": function(event) {
+            event.preventDefault();
+            Places.insert({
+                name: $("#name").val(),
+                votes: 0
+            }, function (err) {
+                if (!err) {
+                    $("input[type=text]").val();
+                }
+            });
+        }
+    });
+}
+
+if (Meteor.isServer) {
+  //Meteor.startup(function () {
+    // code to run on server at startup
+  //});
+    Meteor.publish("places", function() {
+        return Places.find();
+    });
+}
